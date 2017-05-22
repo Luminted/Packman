72ce6f8adb86d0bf547ece8005c019096a8db1dd
ASCYRA\xxx <ascyraleo@gmail.com>
2017-05-22 07:03:08 +0200

    Pacman Map

--- src/js/game/main.js
+++ src/js/game/main.js
@@ -1,4 +1,687 @@
-import b from './b.js';
+import $ from 'jquery';
+import mainloop from 'mainloop.js';
 
-console.log(b);
-var AASSSDDD = [];
+
+//1.
+
+var NONE        = 4,
+    UP          = 3,
+    LEFT        = 2,
+    DOWN        = 1,
+    RIGHT       = 11,
+    WAITING     = 5,
+    PAUSE       = 6,
+    PLAYING     = 7,
+    COUNTDOWN   = 8,
+    EATEN_PAUSE = 9,
+    DYING       = 10,
+    Pacman      = {};
+
+Pacman.FPS = 30;
+
+Pacman.Ghost = function (game, map, colour) {
+
+    var position  = null,
+        direction = null,
+        eatable   = null,
+        eaten     = null,
+        due       = null;
+    
+    function getNewCoord(dir, current) { 
+        
+        var speed  = isVunerable() ? 1 : isHidden() ? 4 : 2,
+            xSpeed = (dir === LEFT && -speed || dir === RIGHT && speed || 0),
+            ySpeed = (dir === DOWN && speed || dir === UP && -speed || 0);
+    
+        return {
+            "x": addBounded(current.x, xSpeed),
+            "y": addBounded(current.y, ySpeed)
+        };
+    };
+
+    function addBounded(x1, x2) { 
+        var rem    = x1 % 10, 
+            result = rem + x2;
+        if (rem !== 0 && result > 10) {
+            return x1 + (10 - rem);
+        } else if(rem > 0 && result < 0) { 
+            return x1 - rem;
+        }
+        return x1 + x2;
+    };
+    
+    function isVunerable() { 
+        return eatable !== null;
+    };
+    
+    function isDangerous() {
+        return eaten === null;
+    };
+
+    function isHidden() { 
+        return eatable === null && eaten !== null;
+    };
+    
+    function getRandomDirection() {
+        var moves = (direction === LEFT || direction === RIGHT) 
+            ? [UP, DOWN] : [LEFT, RIGHT];
+        return moves[Math.floor(Math.random() * 2)];
+    };
+    
+    function reset() {
+        eaten = null;
+        eatable = null;
+        position = {"x": 90, "y": 80};
+        direction = getRandomDirection();
+        due = getRandomDirection();
+    };
+    
+    function onWholeSquare(x) {
+        return x % 10 === 0;
+    };
+    
+    function oppositeDirection(dir) { 
+        return dir === LEFT && RIGHT ||
+            dir === RIGHT && LEFT ||
+            dir === UP && DOWN || UP;
+    };
+
+    function makeEatable() {
+        direction = oppositeDirection(direction);
+        eatable = game.getTick();
+    };
+
+    function eat() { 
+        eatable = null;
+        eaten = game.getTick();
+    };
+
+    function pointToCoord(x) {
+        return Math.round(x / 10);
+    };
+
+    function nextSquare(x, dir) {
+        var rem = x % 10;
+        if (rem === 0) { 
+            return x; 
+        } else if (dir === RIGHT || dir === DOWN) { 
+            return x + (10 - rem);
+        } else {
+            return x - rem;
+        }
+    };
+
+    function onGridSquare(pos) {
+        return onWholeSquare(pos.y) && onWholeSquare(pos.x);
+    };
+
+    function secondsAgo(tick) { 
+        return (game.getTick() - tick) / Pacman.FPS;
+    };
+
+    function getColour() { 
+        if (eatable) { 
+            if (secondsAgo(eatable) > 5) { 
+                return game.getTick() % 20 > 10 ? "#FFFFFF" : "#0000BB";
+            } else { 
+                return "#0000BB";
+            }
+        } else if(eaten) { 
+            return "#222";
+        } 
+        return colour;
+    };
+
+    function draw(ctx) {
+  
+        var s    = map.blockSize, 
+            top  = (position.y/10) * s,
+            left = (position.x/10) * s;
+    
+        if (eatable && secondsAgo(eatable) > 8) {
+            eatable = null;
+        }
+        
+        if (eaten && secondsAgo(eaten) > 3) { 
+            eaten = null;
+        }
+        
+        var tl = left + s;
+        var base = top + s - 3;
+        var inc = s / 10;
+
+        var high = game.getTick() % 10 > 5 ? 3  : -3;
+        var low  = game.getTick() % 10 > 5 ? -3 : 3;
+
+        ctx.fillStyle = getColour();
+        ctx.beginPath();
+
+        ctx.moveTo(left, base);
+
+        ctx.quadraticCurveTo(left, top, left + (s/2),  top);
+        ctx.quadraticCurveTo(left + s, top, left+s,  base);
+        
+		
+        ctx.quadraticCurveTo(tl-(inc*1), base+high, tl - (inc * 2),  base);
+        ctx.quadraticCurveTo(tl-(inc*3), base+low, tl - (inc * 4),  base);
+        ctx.quadraticCurveTo(tl-(inc*5), base+high, tl - (inc * 6),  base);
+        ctx.quadraticCurveTo(tl-(inc*7), base+low, tl - (inc * 8),  base); 
+        ctx.quadraticCurveTo(tl-(inc*9), base+high, tl - (inc * 10), base); 
+
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.fillStyle = "#FFF";
+        ctx.arc(left + 6,top + 6, s / 6, 0, 300, false);
+        ctx.arc((left + s) - 6,top + 6, s / 6, 0, 300, false);
+        ctx.closePath();
+        ctx.fill();
+
+        var f = s / 12;
+        var off = {};
+        off[RIGHT] = [f, 0];
+        off[LEFT]  = [-f, 0];
+        off[UP]    = [0, -f];
+        off[DOWN]  = [0, f];
+
+        ctx.beginPath();
+        ctx.fillStyle = "#000";
+        ctx.arc(left+6+off[direction][0], top+6+off[direction][1], 
+                s / 15, 0, 300, false);
+        ctx.arc((left+s)-6+off[direction][0], top+6+off[direction][1], 
+                s / 15, 0, 300, false);
+        ctx.closePath();
+        ctx.fill();
+
+    };
+
+    function pane(pos) {
+
+        if (pos.y === 100 && pos.x >= 190 && direction === RIGHT) {
+            return {"y": 100, "x": -10};
+        }
+        
+        if (pos.y === 100 && pos.x <= -10 && direction === LEFT) {
+            return position = {"y": 100, "x": 190};
+        }
+
+        return false;
+    };
+    
+    function move(ctx) {
+        
+        var oldPos = position,
+            onGrid = onGridSquare(position),
+            npos   = null;
+        
+        if (due !== direction) {
+            
+            npos = getNewCoord(due, position);
+            
+            if (onGrid &&
+                map.isFloorSpace({
+                    "y":pointToCoord(nextSquare(npos.y, due)),
+                    "x":pointToCoord(nextSquare(npos.x, due))})) {
+                direction = due;
+            } else {
+                npos = null;
+            }
+        }
+        
+        if (npos === null) {
+            npos = getNewCoord(direction, position);
+        }
+        
+        if (onGrid &&
+            map.isWallSpace({
+                "y" : pointToCoord(nextSquare(npos.y, direction)),
+                "x" : pointToCoord(nextSquare(npos.x, direction))
+            })) {
+            
+            due = getRandomDirection();            
+            return move(ctx);
+        }
+
+        position = npos;        
+        
+        var tmp = pane(position);
+        if (tmp) { 
+            position = tmp;
+        }
+        
+        due = getRandomDirection();
+        
+        return {
+            "new" : position,
+            "old" : oldPos
+        };
+    };
+    
+    return {
+        "eat"         : eat,
+        "isVunerable" : isVunerable,
+        "isDangerous" : isDangerous,
+        "makeEatable" : makeEatable,
+        "reset"       : reset,
+        "move"        : move,
+        "draw"        : draw
+    };
+};
+
+
+//2.
+Pacman.User = function (game, map) {
+    
+    var position  = null,
+        direction = null,
+        eaten     = null,
+        due       = null, 
+        lives     = null,
+        score     = 5,
+        keyMap    = {};
+    
+    keyMap[KEY.ARROW_LEFT]  = LEFT;
+    keyMap[KEY.ARROW_UP]    = UP;
+    keyMap[KEY.ARROW_RIGHT] = RIGHT;
+    keyMap[KEY.ARROW_DOWN]  = DOWN;
+
+    function addScore(nScore) { 
+        score += nScore;
+        if (score >= 10000 && score - nScore < 10000) { 
+            lives += 1;
+        }
+    };
+
+    function theScore() { 
+        return score;
+    };
+
+    function loseLife() { 
+        lives -= 1;
+    };
+
+    function getLives() {
+        return lives;
+    };
+
+    function initUser() {
+        score = 0;
+        lives = 3;
+        newLevel();
+    }
+    
+    function newLevel() {
+        resetPosition();
+        eaten = 0;
+    };
+    
+    function resetPosition() {
+        position = {"x": 90, "y": 120};
+        direction = LEFT;
+        due = LEFT;
+    };
+    
+    function reset() {
+        initUser();
+        resetPosition();
+    };        
+    
+    function keyDown(e) {
+        if (typeof keyMap[e.keyCode] !== "undefined") { 
+            due = keyMap[e.keyCode];
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        }
+        return true;
+	};
+
+    function getNewCoord(dir, current) {   
+        return {
+            "x": current.x + (dir === LEFT && -2 || dir === RIGHT && 2 || 0),
+            "y": current.y + (dir === DOWN && 2 || dir === UP    && -2 || 0)
+        };
+    };
+
+    function onWholeSquare(x) {
+        return x % 10 === 0;
+    };
+
+    function pointToCoord(x) {
+        return Math.round(x/10);
+    };
+    
+    function nextSquare(x, dir) {
+        var rem = x % 10;
+        if (rem === 0) { 
+            return x; 
+        } else if (dir === RIGHT || dir === DOWN) { 
+            return x + (10 - rem);
+        } else {
+            return x - rem;
+        }
+    };
+
+    function next(pos, dir) {
+        return {
+            "y" : pointToCoord(nextSquare(pos.y, dir)),
+            "x" : pointToCoord(nextSquare(pos.x, dir)),
+        };                               
+    };
+
+    function onGridSquare(pos) {
+        return onWholeSquare(pos.y) && onWholeSquare(pos.x);
+    };
+
+    function isOnSamePlane(due, dir) { 
+        return ((due === LEFT || due === RIGHT) && 
+                (dir === LEFT || dir === RIGHT)) || 
+            ((due === UP || due === DOWN) && 
+             (dir === UP || dir === DOWN));
+    };
+
+    function move(ctx) {
+        
+        var npos        = null, 
+            nextWhole   = null, 
+            oldPosition = position,
+            block       = null;
+        
+        if (due !== direction) {
+            npos = getNewCoord(due, position);
+            
+            if (isOnSamePlane(due, direction) || 
+                (onGridSquare(position) && 
+                 map.isFloorSpace(next(npos, due)))) {
+                direction = due;
+            } else {
+                npos = null;
+            }
+        }
+
+        if (npos === null) {
+            npos = getNewCoord(direction, position);
+        }
+        
+        if (onGridSquare(position) && map.isWallSpace(next(npos, direction))) {
+            direction = NONE;
+        }
+
+        if (direction === NONE) {
+            return {"new" : position, "old" : position};
+        }
+        
+        if (npos.y === 100 && npos.x >= 190 && direction === RIGHT) {
+            npos = {"y": 100, "x": -10};
+        }
+        
+        if (npos.y === 100 && npos.x <= -12 && direction === LEFT) {
+            npos = {"y": 100, "x": 190};
+        }
+        
+        position = npos;        
+        nextWhole = next(position, direction);
+        
+        block = map.block(nextWhole);        
+        
+        if ((isMidSquare(position.y) || isMidSquare(position.x)) &&
+            block === Pacman.BISCUIT || block === Pacman.PILL) {
+            
+            map.setBlock(nextWhole, Pacman.EMPTY);           
+            addScore((block === Pacman.BISCUIT) ? 10 : 50);
+            eaten += 1;
+            
+            if (eaten === 182) {
+                game.completedLevel();
+            }
+            
+            if (block === Pacman.PILL) { 
+                game.eatenPill();
+            }
+        }   
+                
+        return {
+            "new" : position,
+            "old" : oldPosition
+        };
+    };
+
+    function isMidSquare(x) { 
+        var rem = x % 10;
+        return rem > 3 || rem < 7;
+    };
+
+    function calcAngle(dir, pos) { 
+        if (dir == RIGHT && (pos.x % 10 < 5)) {
+            return {"start":0.25, "end":1.75, "direction": false};
+        } else if (dir === DOWN && (pos.y % 10 < 5)) { 
+            return {"start":0.75, "end":2.25, "direction": false};
+        } else if (dir === UP && (pos.y % 10 < 5)) { 
+            return {"start":1.25, "end":1.75, "direction": true};
+        } else if (dir === LEFT && (pos.x % 10 < 5)) {             
+            return {"start":0.75, "end":1.25, "direction": true};
+        }
+        return {"start":0, "end":2, "direction": false};
+    };
+
+    function drawDead(ctx, amount) { 
+
+        var size = map.blockSize, 
+            half = size / 2;
+
+        if (amount >= 1) { 
+            return;
+        }
+
+        ctx.fillStyle = "#FFFF00";
+        ctx.beginPath();        
+        ctx.moveTo(((position.x/10) * size) + half, 
+                   ((position.y/10) * size) + half);
+        
+        ctx.arc(((position.x/10) * size) + half, 
+                ((position.y/10) * size) + half,
+                half, 0, Math.PI * 2 * amount, true); 
+        
+        ctx.fill();    
+    };
+
+    function draw(ctx) { 
+
+        var s     = map.blockSize, 
+            angle = calcAngle(direction, position);
+
+        ctx.fillStyle = "#FFFF00";
+
+        ctx.beginPath();        
+
+        ctx.moveTo(((position.x/10) * s) + s / 2,
+                   ((position.y/10) * s) + s / 2);
+        
+        ctx.arc(((position.x/10) * s) + s / 2,
+                ((position.y/10) * s) + s / 2,
+                s / 2, Math.PI * angle.start, 
+                Math.PI * angle.end, angle.direction); 
+        
+        ctx.fill();    
+    };
+    
+    initUser();
+
+    return {
+        "draw"          : draw,
+        "drawDead"      : drawDead,
+        "loseLife"      : loseLife,
+        "getLives"      : getLives,
+        "score"         : score,
+        "addScore"      : addScore,
+        "theScore"      : theScore,
+        "keyDown"       : keyDown,
+        "move"          : move,
+        "newLevel"      : newLevel,
+        "reset"         : reset,
+        "resetPosition" : resetPosition
+    };
+};
+
+
+//3.
+Pacman.Map = function (size) {
+    
+    var height    = null, 
+        width     = null, 
+        blockSize = size,
+        pillSize  = 0,
+        map       = null;
+    
+    function withinBounds(y, x) {
+        return y >= 0 && y < height && x >= 0 && x < width;
+    }
+    
+    function isWall(pos) {
+        return withinBounds(pos.y, pos.x) && map[pos.y][pos.x] === Pacman.WALL;
+    }
+    
+    function isFloorSpace(pos) {
+        if (!withinBounds(pos.y, pos.x)) {
+            return false;
+        }
+        var peice = map[pos.y][pos.x];
+        return peice === Pacman.EMPTY || 
+            peice === Pacman.BISCUIT ||
+            peice === Pacman.PILL;
+    }
+    
+    function drawWall(ctx) {
+
+        var i, j, p, line;
+        
+        ctx.strokeStyle = "#0000FF";
+        ctx.lineWidth   = 5;
+        ctx.lineCap     = "round";
+        
+        for (i = 0; i < Pacman.WALLS.length; i += 1) {
+            line = Pacman.WALLS[i];
+            ctx.beginPath();
+
+            for (j = 0; j < line.length; j += 1) {
+
+                p = line[j];
+                
+                if (p.move) {
+                    ctx.moveTo(p.move[0] * blockSize, p.move[1] * blockSize);
+                } else if (p.line) {
+                    ctx.lineTo(p.line[0] * blockSize, p.line[1] * blockSize);
+                } else if (p.curve) {
+                    ctx.quadraticCurveTo(p.curve[0] * blockSize, 
+                                         p.curve[1] * blockSize,
+                                         p.curve[2] * blockSize, 
+                                         p.curve[3] * blockSize);   
+                }
+            }
+            ctx.stroke();
+        }
+    }
+    
+    function reset() {       
+        map    = Pacman.MAP.clone();
+        height = map.length;
+        width  = map[0].length;        
+    };
+
+    function block(pos) {
+        return map[pos.y][pos.x];
+    };
+    
+    function setBlock(pos, type) {
+        map[pos.y][pos.x] = type;
+    };
+
+    function drawPills(ctx) { 
+
+        if (++pillSize > 30) {
+            pillSize = 0;
+        }
+        
+        for (i = 0; i < height; i += 1) {
+		    for (j = 0; j < width; j += 1) {
+                if (map[i][j] === Pacman.PILL) {
+                    ctx.beginPath();
+
+                    ctx.fillStyle = "#000";
+		            ctx.fillRect((j * blockSize), (i * blockSize), 
+                                 blockSize, blockSize);
+
+                    ctx.fillStyle = "#FFF";
+                    ctx.arc((j * blockSize) + blockSize / 2,
+                            (i * blockSize) + blockSize / 2,
+                            Math.abs(5 - (pillSize/3)), 
+                            0, 
+                            Math.PI * 2, false); 
+                    ctx.fill();
+                    ctx.closePath();
+                }
+		    }
+	    }
+    };
+    
+    function draw(ctx) {
+        
+        var i, j, size = blockSize;
+
+        ctx.fillStyle = "#000";
+	    ctx.fillRect(0, 0, width * size, height * size);
+
+        drawWall(ctx);
+        
+        for (i = 0; i < height; i += 1) {
+		    for (j = 0; j < width; j += 1) {
+			    drawBlock(i, j, ctx);
+		    }
+	    }
+    };
+    
+    function drawBlock(y, x, ctx) {
+
+        var layout = map[y][x];
+
+        if (layout === Pacman.PILL) {
+            return;
+        }
+
+        ctx.beginPath();
+        
+        if (layout === Pacman.EMPTY || layout === Pacman.BLOCK || 
+            layout === Pacman.BISCUIT) {
+            
+            ctx.fillStyle = "#000";
+		    ctx.fillRect((x * blockSize), (y * blockSize), 
+                         blockSize, blockSize);
+
+            if (layout === Pacman.BISCUIT) {
+                ctx.fillStyle = "#FFF";
+		        ctx.fillRect((x * blockSize) + (blockSize / 2.5), 
+                             (y * blockSize) + (blockSize / 2.5), 
+                             blockSize / 6, blockSize / 6);
+	        }
+        }
+        ctx.closePath();	 
+    };
+
+    reset();
+    
+    return {
+        "draw"         : draw,
+        "drawBlock"    : drawBlock,
+        "drawPills"    : drawPills,
+        "block"        : block,
+        "setBlock"     : setBlock,
+        "reset"        : reset,
+        "isWallSpace"  : isWall,
+        "isFloorSpace" : isFloorSpace,
+        "height"       : height,
+        "width"        : width,
+        "blockSize"    : blockSize
+    };
+};
+
